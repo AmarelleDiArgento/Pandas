ad17f403c15ae16a1b590876ff8ccb2e1bf99c22
AmarelleDiArgento <ewige.quel@gmail.com>
2021-07-20 22:19:20 -0500

    Agregado el script de pruebas

---
+++ util/testPython.sql
@@ -0,0 +1,11 @@
+CREATE database testingPython;
+
+CREATE table testingPython.dbo.testData(
+		id int identity(1,1),
+		edad int,
+		cm int,
+		pais varchar(2),
+		genero varchar(1),
+		Q1 int,
+		Q2 int,
+);
